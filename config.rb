30ca4c9c3fc8cccac66b863cd41ec305b6a8e9c1
Simon Howard <fraggle@gmail.com>
2013-11-02 02:25:20 -0400

    Add first version of forgehub.

---
+++ config.rb
@@ -0,0 +1,19 @@
+
+# Configuration for forgehub. Configure appropriately.
+
+# Configure GitHub login details:
+
+Octokit.configure do |c|
+  c.login = ''
+  c.password = ''
+end
+
+# Repository to import into:
+
+GITHUB_REPO = ''
+
+# Name of the project on Sourceforge:
+
+SOURCEFORGE_PROJECT = ''
+
+
